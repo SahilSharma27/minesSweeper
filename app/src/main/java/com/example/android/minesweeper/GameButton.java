59b42e8184da8d1e53a1d92da4743bfcd0eebedc
Sahil <27decsahilsharma@gmail.com>
2018-06-25 13:34:47 +0530

    second

---
+++ app/src/main/java/com/example/android/minesweeper/GameButton.java
@@ -0,0 +1,15 @@
+package com.example.android.minesweeper;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatButton;
+import android.widget.GridView;
+
+/**
+ * Created by Sahil Sharma on 24-06-2018.
+ */
+
+public class GameButton extends AppCompatButton {
+    public GameButton(Context context){
+        super(context);
+    }
+}
